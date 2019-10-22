efc43ed72a623c562170f06ed57a6873bddc8ed2
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-22 17:38:57 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,8 @@ class Patient
     @@all
   end
 
-  def new_appointment(date, doctor)
-    Appointment.new(date,doctor,self)
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
